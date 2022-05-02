bf34eca9e1f77c5091e1e77b250607b84685b80f
Ricky Wu <rickywo@gmail.com>
2022-05-02 13:57:33 +1000

    implement scan and total method

--- src/Checkout.py
+++ src/Checkout.py
@@ -9,8 +9,7 @@ class Checkout:
     SUBTOTAL = 'subtotal'
     __order = {}  # a structure for storing purchased items of the order
 
-    # { 'atv': {'quantity': 0, 'subtotal': Decimal('100')}}
-
+    # { 'atv': {'quantity': 0, 'subtotal': 100}}
     def __init__(self, pricing_rule):
         if not isinstance(pricing_rule, PricingRule):
             # raise an exception when argument is not an instance of PricingRule
@@ -22,15 +21,24 @@ class Checkout:
     # param: item is an instance of Product class
     # output: example: 'atv', 4, 1299.99
     def scan(self, item):
+        if not isinstance(item, Product):
+            print('No such item')
+            return
         sku = item.get_sku()
-        num_of_item = 0
-        subtotal = 0
+        if sku in self.__order:
+            self.__order[sku][self.QUANTITY] += 1
+        else:
+            self.__order[sku] = {self.QUANTITY: 1}
+        num_of_item = self.__order[sku][self.QUANTITY]
+        subtotal = self.__rule.apply_rule(sku, num_of_item, self.__order)
         self.__order[sku][self.SUBTOTAL] = subtotal
         return sku, num_of_item, subtotal
 
     # scan
     # param:
-    # output: 1299.99
+    # output: example: 1299.99
     def total(self):
         total = 0
+        for k, v in self.__order.items():
+            total += round(v[self.SUBTOTAL], 2)
         return total
